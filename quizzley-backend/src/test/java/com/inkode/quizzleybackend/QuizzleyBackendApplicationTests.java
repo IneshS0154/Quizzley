8bd88b34ca76d76e03863b3312ed34fdf5bcdf30
IneshS <71919137+IneshS0154@users.noreply.github.com>
2026-05-30 22:21:36 +0530

    Initial Commit

---
+++ quizzley-backend/src/test/java/com/inkode/quizzleybackend/QuizzleyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inkode.quizzleybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzleyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ quizzley-database/README.md
@@ -0,0 +1,4 @@
+quizleyy-database/
+├── 01-schema/       <-- Akash, Bhanuka & Manul's CREATE TABLE scripts
+├── 02-constraints/  <-- Foreign keys, unique constraints, and indexes
+└── 03-seeding/      <-- Mock data insert scripts (e.g., sample questions)
